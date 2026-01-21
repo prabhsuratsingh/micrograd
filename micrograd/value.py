e9141a3efaaed5973edb1e86104f209b7ba34153
prabhsuratsingh <prabhsurat17@gmail.com>
2026-01-21 17:21:15 +0530

    create value data structure

---
+++ micrograd/value.py
@@ -0,0 +1,16 @@
+class Value:
+    def __init__(self, data, _children=(), _op=''):
+        self.data = data
+        self._prev = set(_children)
+        self._op = _op
+
+    def __repr__(self):
+        return f"Value(data={self.data})"
+    
+    def __add__(self, other):
+        out = Value(self.data + other.data, (self, other), '+')
+        return out
+    
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), '*')
+        return out
