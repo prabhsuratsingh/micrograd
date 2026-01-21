e24f611d3629cbcf2b87c7c31ffad3ff499b92c3
prabhsuratsingh <prabhsurat17@gmail.com>
2026-01-21 18:05:05 +0530

    add DAG visualizing function

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from micrograd!")
+from micrograd.value import Value
+from micrograd.visualize import draw_dot
 
 
-if __name__ == "__main__":
-    main()
+a = Value(2.0, label='a')
+b = Value(-3.0, label='b')
+c = Value(10.0, label='c')
+
+e = a*b; e.label='e'
+d = e+ c; d.label='d'
+f = Value(-2.0, label='f')
+L = d * f; L.label='L'
+
+
+dot = draw_dot(L)
+dot.render("graph", view=True)
