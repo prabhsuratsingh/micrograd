7c9165df8526f9dca721622605d499471cf41a98
prabhsuratsingh <prabhsurat17@gmail.com>
2026-01-29 19:01:06 +0530

    create Neuron class:

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
+from micrograd.neuron import Neuron
 from micrograd.value import Value
 from micrograd.visualize import draw_dot
 
 
-x1 = Value(2.0, label='x1')
-x2 = Value(0.0, label='x2')
-w1 = Value(-3.0, label='w1')
-w2 = Value(1.0, label='w2')
-b = Value(6.8813735870195432, label='b')
+# x1 = Value(2.0, label='x1')
+# x2 = Value(0.0, label='x2')
+# w1 = Value(-3.0, label='w1')
+# w2 = Value(1.0, label='w2')
+# b = Value(6.8813735870195432, label='b')
 
-x1w1 = x1 * w1; x1w1.label = "x1w1"
-x2w2 = x2 * w2; x2w2.label = "x2w2"
-x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = "x1w1 + x2w2"
+# x1w1 = x1 * w1; x1w1.label = "x1w1"
+# x2w2 = x2 * w2; x2w2.label = "x2w2"
+# x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = "x1w1 + x2w2"
 
-n = x1w1x2w2 + b; n.label = "n"
-
-
-# o = n.tanh()
-
-e = (2*n).exp()
-o = (e-1)/(e+1)
-o.label = 'o'
+# n = x1w1x2w2 + b; n.label = "n"
 
 
-o.backward()
-dot = draw_dot(o)
-dot.render("graph", view=True)
+# # o = n.tanh()
 
+# e = (2*n).exp()
+# o = (e-1)/(e+1)
+# o.label = 'o'
+
+
+# o.backward()
+# dot = draw_dot(o)
+# dot.render("graph", view=True)
+
+x = [2.0, 3.0]
+n = Neuron(2)
+print(n(x))
